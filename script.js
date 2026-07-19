5944426394fe122b9f4acbffe3cd6fb1b98ff14d
imranfanani8-crypto <imranfanani8@gmail.com>
2026-07-19 18:09:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -286,3 +286,21 @@ ${messaggio}`;
     transform:scale(1.1);
     }
     
+    const toggle=document.querySelector(".menu-toggle");
+const menu=document.querySelector(".menu");
+
+toggle.addEventListener("click",()=>{
+
+    menu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".menu a").forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        menu.classList.remove("active");
+
+    });
+
+});
